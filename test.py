d2c089f06dc54bf4f7efeacfeab95a6963c4ad9e
Arlo Blanco <arloblanco7@gmail.com>
2021-02-02 20:40:41 -0500

    removed etc

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-# msg = input("what do you like to say: ")
-
-# print(sys.version)
-# print(sys.executable)
-print("hello")
-
-
-def greet():
-    return
-
-
-#r = requests.get('https://coreyms.com')
-# print(r.status_code)
+r = requests.get('https://coreyms.com')
+print(r.status_code)
+print(r.ok)
