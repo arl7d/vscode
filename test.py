bfa2bce4f1d4275b267c6c460249e2c2cc134ce3
Arlo Blanco <arloblanco7@gmail.com>
2021-02-02 20:36:36 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# msg = input("what do you like to say: ")
+
+# print(sys.version)
+# print(sys.executable)
+print("hello")
+
+
+def greet():
+    return
+
+
+#r = requests.get('https://coreyms.com')
+# print(r.status_code)
